d93ffbac099c697f99b0914d0e28a45de58646fb
rejigtian <875283604@qq.com>
2026-06-16 16:27:03 +0800

    Bump Android app version to 1.0.1 (versionCode 2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.dream.smart_androidbot"
         minSdk = 30
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
